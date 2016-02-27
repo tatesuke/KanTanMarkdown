817bf15aa6fdfeb4a8b8eee7c6d1c49cc2a24fdf
tatesuke <acousticdays@gmail.com>
2016-02-27 17:53:54 +0900

    アップデートするとエディションの情報が消えてしまうバグを修正

--- src/kantanUpdate.js
+++ src/kantanUpdate.js
@@ -1,7 +1,8 @@
 kantanUpdate({
-	"newVersion": <inline src="kantanVersion.js" />,
+	"newVersion": "<inline src="kantanVersion.txt" />",
 	"ktmString": <inline src="ktmString.js" />,
 	"doUpdate": function() {
+		var kantanVersion = document.getElementById("kantanVersion").value;
 		if (kantanVersion == this.newVersion) {
 			alert("お使いのKantanMarkdownは最新です。アップデートの必要はありません。");
 			return;
@@ -26,11 +27,14 @@ kantanUpdate({
 		// アップデート前の値を記憶しておく
 		var fileListElement = document.querySelector("ul#fileList");
 		var originalMarkdown = document.querySelector("textarea#editor").value;
-
+		
+		// エディションを読み込み
+		var kantanEdition = document.querySelector("#kantanEdition").value;
+		
 		// メモリリーク対策のためにイベントをすべて消す。
 		removeAllEvents();
 		
-		// liteバージョンではhljsのcssを消す
+		// liteエディションではhljsのcssを消す
 		var newHtml = this.ktmString;
 		if (kantanEdition == "lite") {
 			newHtml = newHtml.replace(/\/\* START_HLJS_CSS \*\/[\s|\S]+\/\* END_HLJS_CSS \*\//, "");
@@ -39,6 +43,10 @@ kantanUpdate({
 		// 本体をアップデート
 		var html = document.querySelector("html");
 		html.innerHTML = newHtml;
+		
+		// バージョン・エディション更新
+		updateKantanVersion(this.newVersion);
+		updateKantanEdition(kantanEdition);
 		
 		// innerHTMLではscriptタグが実行されないのでいったん外して付け直す
 		var scriptElements = document.querySelectorAll("script");
@@ -69,10 +77,6 @@ kantanUpdate({
 			
 			document.querySelector("body").appendChild(newScript);
 		}
-		
-		// バージョン更新
-		kantanVersion = this.newVersion;
-		updateVersion();
 		
 		// 添付ファイル引継ぎ
 		if (dialogResult.attach == true) {
