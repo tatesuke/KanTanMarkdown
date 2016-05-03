8cc4acd9d412406ade013e2c087c8ee06c218b6d
tatesuke <acousticdays@gmail.com>
2016-05-03 12:41:59 +0900

    保存機能をリファクタリング

--- src/js/kantanCoreSave.js
+++ src/js/kantanCoreSave.js
@@ -1,31 +1,35 @@
-/* アップデート機能 */
-(function(prototype, ktm){
+/* 保存機能 */
+(function KtmCoreSave(prototype, ktm){
+	
+	const $previewerStyle   = document.getElementById("previewerStyle");
+	const $messageArea      = document.getElementById("messageArea");
+	const $wrapper          = document.getElementById("wrapper");
+	const $editor           = document.getElementById("editor");
+	const $cssEditor        = document.getElementById("cssEditor");
+	const $previewer        = document.getElementById("previewer");
+	const $updateScriptArea = document.getElementById("updateScriptArea");
+	
+	var _saved         = true;
+	var _contentAtSave = $editor.value;
 
 	document.addEventListener('DOMContentLoaded', function() {
-	
+		on("#saveButton", "click", ktm.save);
+		window.onbeforeunload = onbeforeunload;
 	});
-
-	prototype.isSaved = function() {
-		return saved;
-	}
 	
-	
-	/* 保存 */
-	var contentAtSave = editor.value;
-	on("#saveButton", "click", save);
-	function save() {
-		var html = getHTMLForSave();
-		
+	prototype.save = function() {
+		var html = ktm.getHTMLForSave();
 		var blob = new Blob([html]);
+		
 		if (window.navigator.msSaveBlob) {
 			/* for IE */
 			window.navigator.msSaveBlob(blob, document.title + ".html");
 		} else {
-			var url = window.URL || window.webkitURL;		
+			var url     = window.URL || window.webkitURL;		
 			var blobURL = url.createObjectURL(blob);
-			var a = document.createElement('a');
-			a.download = document.title.replace(/^\* /, "") + ".html";
-			a.href = blobURL;
+			var a       = document.createElement('a');
+			a.download  = document.title.replace(/^\* /, "") + ".html";
+			a.href      = blobURL;
 			
 			// firefoxでa.click()が効かないため無理やりclickイベントをディスパッチする
 			var ev = document.createEvent("MouseEvents");
@@ -35,30 +39,28 @@
 		}
 	}
 
-	function getHTMLForSave() {
-		var wrapperScrollTop = document.getElementById("wrapper").scrollTop;
-		var previewerScrollTop = document.getElementById("previewer").scrollTop;
+	prototype.getHTMLForSave = function() {
+		var wrapperScrollTop   = $wrapper.scrollTop;
+		var previewerScrollTop = $previewer.scrollTop;
 		
-		// アップデート用のscriptタグを消す
-		document.querySelector("#updateScriptArea").innerHTML = "";
+		// アップデート用のscriptタグは差分の原因になるので消す
+		$updateScriptArea.innerHTML = "";
 		
 		// テキストエリアは値を入れなおさないと保存されない。
-		var editor = document.getElementById("editor");
-		editor.innerHTML = editor.value.replace(/</g, "&lt;");
+		$editor.innerHTML    = $editor.value.replace(/</g, "&lt;");
+		$cssEditor.innerHTML = $cssEditor.value;
 		
-		var cssEditor = document.getElementById("cssEditor");
-		cssEditor.innerHTML = cssEditor.value;
-		
+		// プレビューモードで保存する
 		var toggleFlag = ktm.isEditMode();
 		if (toggleFlag == true) {
 			ktm.toggleMode();
 		}
-		addClass(document.querySelector("body"), "initialState");
+		addClass(document.body, "initialState");
 		
 		/* ファイルの肥大化を防ぐため中身を消去 */
-		document.getElementById("previewer").innerHTML = "";
-		document.getElementById("messageArea").innerHTML = "";
-		document.getElementById("previewerStyle").innerHTML="";
+		$previewer.innerHTML      = "";
+		$messageArea.innerHTML    = "";
+		$previewerStyle.innerHTML = "";
 		
 		// 不要なdiffの原因となるスタイルなどを削除
 		if (document.title.match(/^\* .*/)) {
@@ -71,44 +73,40 @@
 			stylee.removeAttribute("style");
 		}
 		
-		document.querySelector("#previewer").removeAttribute("class");
+		$previewer.removeAttribute("class");
 		
 		// HTML生成
-		var html = "<!doctype html>\n<html>\n";
-		html += document.getElementsByTagName("html")[0].innerHTML;
-		var i = html.lastIndexOf("/script>"); // bug fix #1 #39
-		html = html.substring(0, i);
-		html += "/script>\n</body>\n</html>";
+		// bug fix #1 #39
+		var html  = "<!doctype html>\n<html>\n";
+		html     += document.getElementsByTagName("html")[0].innerHTML;
+		html      = html.substring(0, html.lastIndexOf("/script>"));
+		html     += "/script>\n</body>\n</html>";
 		
-		contentAtSave = editor.value;
-		saved = true;
+		_contentAtSave = $editor.value;
+		_saved = true;
 		ktm.doPreview();
 		
-		removeClass(document.querySelector("body"), "initialState");
+		removeClass(document.body, "initialState");
 		if (toggleFlag == true) {
 			ktm.toggleMode();
 		}
 		
-		document.getElementById("previewer").scrollTop = previewerScrollTop;
-		document.getElementById("wrapper").scrollTop = wrapperScrollTop;
+		$previewer.scrollTop = previewerScrollTop;
+		$wrapper.scrollTop   = wrapperScrollTop;
+		
 		return html;
 	}
-
-	/* 更新時、終了時の警告 */
-	var saved = true;
-	window.onbeforeunload = function() {
-		if (!saved) {
-			return "ドキュメントが保存されていません。"
-		}
+	
+	prototype.isSaved = function() {
+		return _saved;
+	}
+	prototype.updateSavedFlag = function() {
+		_saved = (_contentAtSave == $editor.value);
 	}
 
-	on("#editor", "input", ktm.updateSavedFlag);
-	
-	prototype.updateSavedFlag = function() {
-		if (contentAtSave == document.getElementById("editor").value) {
-			saved = true;
-		} else {
-			saved = false;
+	function onbeforeunload() {
+		if (!_saved) {
+			return "ドキュメントが保存されていません。"
 		}
 	}
 
